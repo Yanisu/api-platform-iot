5e048e2b3cc2039b864230d42a23d909f80a95d9
Mattéo TERRA <74179030+Matteo-terra@users.noreply.github.com>
2022-03-16 11:15:50 +0100

    configure Firebase connection

--- socket/server.js
+++ socket/server.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
-// var storage = require("./storage")
+var storage = require("./storage")
 require('dotenv').config()
 
 
@@ -33,13 +33,23 @@ serialport.on("open", function () {
 
   frame_obj = { // AT Request to be sent
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
-    destination64: "FFFFFFFFFFFFFFFF",
-    command: "NI",
-    commandParameter: [],
+    destination64: "00 13 A2 00 41 A7 29 56",
+    command: "D1",
+    commandParameter: [03],
   };
   xbeeAPI.builder.write(frame_obj);
 
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination64: "00 13 A2 00 41 A7 29 56",
+    command: "IC",
+    commandParameter: [2],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+
 });
+
 
 // All frames parsed by the XBee will be emitted here
 
@@ -50,7 +60,7 @@ xbeeAPI.parser.on("data", function (frame) {
   //on new device is joined, register it
 
   //on packet received, dispatch event
-  //let dataReceived = String.fromCharCode.apply(null, frame.data);
+  let dataReceived = String.fromCharCode.apply(null, frame.data);
   if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
@@ -59,15 +69,16 @@ xbeeAPI.parser.on("data", function (frame) {
   }
 
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
+     let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
-    // storage.registerSensor(frame.remote64)
+    storage.registerSensor(frame.remote64)
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
     console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
-    // storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
+    console.log(frame.digitalSamples.DIO1)
+    
+    storage.registerSample(frame.remote64,frame.digitalSamples.DIO1 )
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE")
